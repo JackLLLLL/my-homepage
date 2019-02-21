c4bbc990d0bab04fd390b6e202978c92ac1777eb
JackLLLLL <jiakangl@sfu.ca>
2019-02-21 01:35:07 -0800

    forget build files

---
+++ build/precache-manifest.33658c37177572999a961b415f7cbf6c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca9cbb05b5a773dc959c9873ccfd215d",
+    "url": "./static/media/planet.ca9cbb05.ico"
+  },
+  {
+    "revision": "69e8310cb52e39548add",
+    "url": "./static/css/main.13b5281b.chunk.css"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "691eb87dc8c1cc8bdc54f70a853596fe",
+    "url": "./static/media/rocket.691eb87d.svg"
+  },
+  {
+    "revision": "531f57acc13de5da3006",
+    "url": "./static/js/2.531f57ac.chunk.js"
+  },
+  {
+    "revision": "aaec2ca17cb42ee9c6e47c31a96840f1",
+    "url": "./static/media/background.aaec2ca1.jpg"
+  },
+  {
+    "revision": "69e8310cb52e39548add",
+    "url": "./static/js/main.69e8310c.chunk.js"
+  },
+  {
+    "revision": "aa8647890eb77d69ef93baca19b286e9",
+    "url": "./static/media/resume.aa864789.pdf"
+  },
+  {
+    "revision": "901055e4cf0172bc7c6417db590a0c90",
+    "url": "./static/media/qrcode.901055e4.png"
+  },
+  {
+    "revision": "bb9e213a21470d32387d0d76b9f3bea5",
+    "url": "./static/media/wechat.bb9e213a.jpg"
+  },
+  {
+    "revision": "531f57acc13de5da3006",
+    "url": "./static/css/2.9cdf7362.chunk.css"
+  },
+  {
+    "revision": "28847205276e4506d77070d36b918d74",
+    "url": "./index.html"
+  }
+];
